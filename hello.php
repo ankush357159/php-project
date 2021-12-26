9a04fe75903feb77fab827dd33384f9cd26b7721
Ankush <ankush357159@gmail.com>
2021-12-26 22:11:57 +0530

    First php commit

---
+++ hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Projects</title>
+</head>
+
+<body>
+    <h2>This is sample PHP File</h2>
+    <?php
+    echo "Hello world!";
+    ?>
+</body>
+
+</html>
